da79b6690f22384f0730ea04d011711375cc6326
CarlEverest78 <thepythoneer@gmail.com>
2022-08-13 11:12:37 +0200

    add proper google maps location urls

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ const data = [
     {
         title: "Sydney Opera House",
         location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://goo.gl/maps/AKNZYhL1epT65UJu7",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
@@ -20,7 +20,7 @@ const data = [
     {
         title: "Geirangerfjord",
         location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://goo.gl/maps/zQJkR2XhouvQ8DWn6",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
